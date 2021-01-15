73c8a282c82855d3d22676046da8f3ab2ebd2770
coolbas <coolbas87@yandex.ua>
2021-01-16 01:44:46 +0200

    Added daterangepicker to history component

--- src/app/temp-history/temp-history.component.ts
+++ src/app/temp-history/temp-history.component.ts
@@ -13,8 +13,10 @@ export class TempHistoryComponent implements OnInit {
   @Input() from;
   @Input() to;
   @Input() id;
+  data: Date[];
 
   readonly  ROW_CLASS_NAME = 'table-';
+  readonly  DATE_FORMAT = 'YYYY-MM-DD';
   history: TempHistoryItem[];
 
   constructor(private tempHistoryService: TempHistoryService, private route: ActivatedRoute) { }
@@ -39,9 +41,13 @@ export class TempHistoryComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.from = !this.from ? moment().subtract(7, 'days').format('YYYY-MM-DD') : this.from;
-    this.to = !this.to ? moment().format('YYYY-MM-DD') : this.to;
+    this.setDefaultDates();
     this.getHistory();
+  }
+
+  private setDefaultDates(): void {
+    this.from = !this.from ? moment().subtract(7, 'days').format(this.DATE_FORMAT) : this.from;
+    this.to = !this.to ? moment().format(this.DATE_FORMAT) : this.to;
   }
 
   private getHistory() {
@@ -50,14 +56,27 @@ export class TempHistoryComponent implements OnInit {
       this.tempHistoryService.getTempHistory(this.from, this.to)
         .subscribe(history => {
           this.history = history;
-          console.log(history);
         });
     } else {
       this.tempHistoryService.getTempHistoryByID(+id, this.from, this.to)
         .subscribe(history => {
           this.history = history;
-          console.log(history);
         });
+    }
+  }
+
+  public onShowClick(): void {
+    this.getHistory();
+  }
+
+  public onDateChange(dates: Date[]): void {
+    if ((!dates) || (dates.length !== 2)) {
+      this.from = null;
+      this.to = null;
+      this.setDefaultDates();
+    } else {
+      this.from = moment(dates[0]).format(this.DATE_FORMAT);
+      this.to = moment(dates[1]).format(this.DATE_FORMAT);
     }
   }
 }
